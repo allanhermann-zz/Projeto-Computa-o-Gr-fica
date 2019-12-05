f19c819d5016ff389d201bed96cbe9305d2aa2fe
allanhermann <allan.hermann@gmail.com>
2019-12-05 00:18:00 -0400

    Versão Entregue

---
+++ Assets/CameraController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject player;
+
+    public Transform Obstruction;
+    float zoomSpeed = 2f;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+        ViewObstructed();
+    }
+
+    void ViewObstructed()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, player.transform.position - transform.position, out hit, 4.5f))
+        {
+            if (hit.collider.gameObject.tag != "Player")
+            {
+                Obstruction = hit.transform;
+                Obstruction.gameObject.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
+
+                if (Vector3.Distance(Obstruction.position, transform.position) >= 3f && Vector3.Distance(transform.position, player.transform.position) >= 1.5f)
+                    transform.Translate(Vector3.forward * zoomSpeed * Time.deltaTime);
+            }
+            else
+            {
+                Obstruction.gameObject.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+                if (Vector3.Distance(transform.position, player.transform.position) < 4.5f)
+                    transform.Translate(Vector3.back * zoomSpeed * Time.deltaTime);
+            }
+        }
+    }
+}
---
+++ Assets/LightDisable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LightDisable : MonoBehaviour
+{
+    public Light myLight;
+
+    private void OnTriggerEnter()
+    {
+        Debug.Log("Entrou na área do Trigger");
+        myLight.enabled = !myLight.enabled;
+    }
+
+    private void OnTriggerExit()
+    {
+        Debug.Log("Saiu da área do Trigger");
+    }
+
+
+}
